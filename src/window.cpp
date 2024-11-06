973f6f360b71001a8e88eb5205c8444b7edc77db
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-06 20:46:55 +0600

    refactor: replace hard-coded overlay values with constants
    - Replaced hard-coded overlay dimensions and offsets with constants:
        * `OVERLAY_Y_OFFSET`
        * `OVERLAY_X_OFFSET`
        * `OVERLAY_Y_DIFF`
        * `OVERLAY_X_DIFF`
    
    This change improves maintainability and clarity.

--- src/window.cpp
+++ src/window.cpp
@@ -1,8 +1,10 @@
 #include "window.hpp"
+#include "constants.hpp"
 
 PrettyWindow::PrettyWindow(int row, int col, int height, int width)
     : _background(newwin(height, width, row, col), delete_window)
-    , _overlay(derwin(_background.get(), row-4, col-2, 3, 1), delete_window)
+    , _overlay(derwin(_background.get(),
+               height - OVERLAY_Y_DIFF, width - OVERLAY_X_DIFF, OVERLAY_Y_OFFSET, OVERLAY_X_OFFSET), delete_window)
     , _panel(new_panel(_background.get()), delete_panel) {
 
     box(_background.get(), 0, 0);
