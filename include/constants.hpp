061ff58a46a7ed1f1eb6aeb4464736a2f34b85bf
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-06 20:05:05 +0600

    fix(constants): remove redundant `const` qualifier
    - Simplified the constant declarations by removing unnecessary `const` keyword.

--- include/constants.hpp
+++ include/constants.hpp
@@ -5,7 +5,7 @@
 
 #include <string>
 
-constexpr const int MIN_WIN_WIDTH = 8;
-constexpr const int MIN_WIN_HEIGHT = 3;
+constexpr int MIN_WIN_WIDTH = 8;
+constexpr int MIN_WIN_HEIGHT = 3;
 
 #endif //SAPHY_CONSTANTS_HPP
