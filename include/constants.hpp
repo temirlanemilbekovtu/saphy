5e68cb07ec5431b9e0c2033528d9550384d24682
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-06 19:31:59 +0600

    feat: add header file for defining custom constants
    - Created `constants.hpp` to store project-specific `constexpr` constants.
    - Added `MIN_WIN_WIDTH` and `MIN_WIN_HEIGHT` to enforce minimum window dimensions.
    
    This file centralizes key constants, improving maintainability and readability.

---
+++ include/constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef SAPHY_CONSTANTS_HPP
+#define SAPHY_CONSTANTS_HPP
+
+#include <string>
+
+constexpr const int MIN_WIN_WIDTH = 8;
+constexpr const int MIN_WIN_HEIGHT = 3;
+
+#endif //SAPHY_CONSTANTS_HPP
