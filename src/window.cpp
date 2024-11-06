9aeb1fc7566fa7f7abbd786cb8f755745737b99e
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-06 20:54:10 +0600

    fix: add validation for window dimensions in `PrettyWindow` constructor
    - Introduced a check to ensure the window height and width meet the minimum required dimensions:
        * `MIN_WIN_HEIGHT` for height
        * `MIN_WIN_WIDTH` for width
    - Throws an `invalid_argument` exception if the dimensions are too small.

--- src/window.cpp
+++ src/window.cpp
@@ -2,10 +2,21 @@
 #include "constants.hpp"
 
 PrettyWindow::PrettyWindow(int row, int col, int height, int width)
-    : _background(newwin(height, width, row, col), delete_window)
-    , _overlay(derwin(_background.get(),
-               height - OVERLAY_Y_DIFF, width - OVERLAY_X_DIFF, OVERLAY_Y_OFFSET, OVERLAY_X_OFFSET), delete_window)
-    , _panel(new_panel(_background.get()), delete_panel) {
+    : _background(nullptr, delete_window)
+    , _overlay(nullptr, delete_window)
+    , _panel(nullptr, delete_panel) {
+
+    if (height < MIN_WIN_HEIGHT || width < MIN_WIN_WIDTH) {
+        throw std::invalid_argument(
+                "Window dimensions cannot be less then "
+                + std::to_string(MIN_WIN_HEIGHT) + " and "
+                + std::to_string(MIN_WIN_WIDTH) + " for height and width, respectively.");
+    }
+
+    _background.reset(newwin(height, width, row, col));
+    _overlay.reset(derwin(_background.get(),
+                          height - OVERLAY_Y_DIFF, width - OVERLAY_X_DIFF, OVERLAY_Y_OFFSET, OVERLAY_X_OFFSET));
+    _panel.reset(new_panel(_background.get()));
 
     box(_background.get(), 0, 0);
 
