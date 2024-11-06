f71c23313c08cedd8e017d5ffa6f1ab39cb99432
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-06 21:22:49 +0600

    feat: add constants for label positioning and ellipsis handling
    - Introduced `LABEL_Y`, `LABEL_X`, `LABEL_MARGIN`, and `ELLIPSIS` constants to manage label display and positioning.

--- include/constants.hpp
+++ include/constants.hpp
@@ -13,4 +13,10 @@ constexpr int OVERLAY_X_DIFF = 2;
 constexpr int MIN_WIN_WIDTH = 8;
 constexpr int MIN_WIN_HEIGHT = 3;
 
+constexpr int LABEL_Y = 0;
+constexpr int LABEL_X = 1;
+constexpr int LABEL_MARGIN = 2;
+
+constexpr std::string ELLIPSIS = "...";
+
 #endif //SAPHY_CONSTANTS_HPP
